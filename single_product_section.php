36799aa9007e25c77b5d449426d55cf9f82e42d8
xianxinl <31662429+xianxinl@users.noreply.github.com>
2019-10-14 20:49:02 -0400

    Update single_product_section.php
    1. Capitalize each variable 
    2. dynamic display for image && product_description

--- single_product_section.php
+++ single_product_section.php
@@ -1,15 +1,16 @@
 <a href="single.html">
   <li>
-  <img src="images/m8.jpg" title="" alt="" />
+  <img src=<?php echo $Image_location ?> title="" alt="" />
   <section class="list-left">
   <h5 class="title"><?php echo $Product_Name; ?></h5>
-  <span Class="product_decription"> </span>
+  <span Class="Product_description"><?php echo 	$Product_description ?> </span>
   <span class="adprice"> <?php  echo $Price; ?> </span>
   </section>
   <section class="list-right">
-  <span class="date"><?php echo $release_date; ?></span>
+  <span class="date"><?php echo $Release_date; ?></span>
   <span Class="contact email"> xxxxx@buffalo.edu</span>
   <span Class="contact phone number"> xxxx-xxx-xx67</span>
+  <?php echo $id ?>
   </section>
   <div class="clearfix"></div>
   </li>
