9a4acac0292f36176cb2dc0e6ac9f447fc6710cd
xianxinl <31662429+xianxinl@users.noreply.github.com>
2019-10-16 20:54:16 -0400

    Update single_product_section.php
    Change Email, Phone number, Image_Location from constant to variable 
    able to display item information in database

--- single_product_section.php
+++ single_product_section.php
@@ -1,15 +1,16 @@
+
 <a href="single.html">
   <li>
-  <img src="images/m8.jpg" title="" alt="" />
+  <img src=<?php echo $Image_location ?> title="" alt="" />
   <section class="list-left">
-  <h5 class="title"><?php echo $Product_Name; ?></h5>
-  <span Class="product_decription"> </span>
-  <span class="adprice"> <?php  echo $Price; ?> </span>
+  <h5 class="title"><?php echo $Product_Name ?></h5>
+  <span Class="Product_description"><?php echo 	$Product_description ?> </span>
+  <span class="adprice"> <?php  echo $Price ?> </span>
   </section>
   <section class="list-right">
-  <span class="date"><?php echo $release_date; ?></span>
-  <span Class="contact email"> xxxxx@buffalo.edu</span>
-  <span Class="contact phone number"> xxxx-xxx-xx67</span>
+  <span class="date"><?php echo $Release_date ?></span>
+  <span Class="contact email"><?php echo $Email ?></span>
+  <span Class="contact phone number"> <?php echo $Phone_number ?></span>
   </section>
   <div class="clearfix"></div>
   </li>
