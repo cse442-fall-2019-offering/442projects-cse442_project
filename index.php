17ad7d1357bf5b14ce5153548ba41cd5d83312f7
xianxinl <31662429+xianxinl@users.noreply.github.com>
2019-10-16 20:52:18 -0400

    Update index.php
    1. display username at the top-right corner when login in (Change "Login in " to username)
    2. add "Sign Out" to remove username from top-right corner, link to "Sign_Out.php"
    3. read all From database, new variable: Image_location, Phone_number, Email

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <?php
-  require_once('setting/server_config.php');
+  require_once('setting/local_host_config.php');
+  session_start();
  ?>
 <html lang="en">
 <head>
@@ -13,8 +14,19 @@
 		<div class="w3ls-header"><!--header-one-->
 			<div class="w3ls-header-right">
 				<ul>
+          <!-- click to sign out account -->
+          <li class="dropdown head-dpdn">
+            <a href="setting/Sign_Out.php"><i aria-hidden="true"></i> Sign Out</a>
+          </li>
+          <!-- sign out end -->
 					<li class="dropdown head-dpdn">
-						<a href="signin_front.php" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i> Sign In</a>
+            <!-- replace login in by username when logined in !-->
+						<a href="signin_front.php" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i>
+              <?php if (isset($_SESSION['username']))
+    				{
+        		echo $_SESSION['username'];
+    			} else echo"Sign In"?> </a>
+          <!-- username display end !-->
 					</li>
 					<li class="dropdown head-dpdn">
 						<a href="help.html"><i class="fa fa-question-circle" aria-hidden="true"></i> Help</a>
@@ -67,13 +79,17 @@
   display all products in database to front site with its' information
   in section format-->
 								<?php
-                $query = "SELECT id, Product_Name, Price, release_date FROM product";
+                $query = "SELECT * FROM product ORDER BY Release_date DESC";
                 $result = mysqli_query($conn, $query);
                 while($row = mysqli_fetch_assoc($result)){
                   $id =$row['id'];
                   $Product_Name = $row['Product_Name'];
                   $Price = $row['Price'];
-                  $release_date = $row['release_date'];
+                  $Release_date = $row['Release_date'];
+                  $Image_location = $row['Image_location'];
+                  $Phone_number = $row['Phone_number'];
+                  $Email = $row['Email'];
+                  $Product_description = $row['Product_description'];
                     include 'single_product_section.php';
                 }
 
