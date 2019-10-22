1a691e46665ed52d420880c8d9999a9675d96726
xianxinl <31662429+xianxinl@users.noreply.github.com>
2019-10-21 22:01:13 -0400

    Add files via upload
    handle each product_id 
    adding a delete button to dele_post.php

---
+++ single_product_section.php
@@ -0,0 +1,19 @@
+      <a>
+        <li>
+          <img src=<?php echo "images/".$Image ?> title="" alt="" />
+          <section class="list-left">
+            <h5 class="title"><?php echo $Product_Name ?></h5>
+            <span Class="Product_description"><?php echo 	$Product_description ?> </span>
+            <span class="adprice"> <?php  echo $Price ?> </span>
+            </section>
+            <section class="list-right">
+              <form class="dele_form" method="GET" action="dele_post.php">
+                <input type="hidden" name="Product_ID" value =<?php echo $Product_ID ?> >
+                <button>Delete</button>
+              <span class="date"><?php echo $Release_date ?></span>
+              <br><span Class="Email"><?php echo $Email ?></span> <br>
+              <span Class="contact_phone_number"> <?php echo $Phone_number ?></span>
+            </section>
+            <div class="clearfix"></div>
+          </li>
+          </a>
