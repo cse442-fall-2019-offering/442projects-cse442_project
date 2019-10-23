ec0a6b1f7a2c8a554d9fedebf30e67585af674bb
Junlong Ye <junlongfighting@gmail.com>
2019-10-22 22:09:23 -0400

    Revert "Login in "

--- single_product_section.php
+++ single_product_section.php
@@ -1,18 +1,16 @@
-
 <a href="single.html">
   <li>
   <img src=<?php echo $Image_location ?> title="" alt="" />
   <section class="list-left">
-
-  <h5 class="title"><?php echo $Product_Name ?></h5>
+  <h5 class="title"><?php echo $Product_Name; ?></h5>
   <span Class="Product_description"><?php echo 	$Product_description ?> </span>
-  <span class="adprice"> <?php  echo $Price ?> </span>
+  <span class="adprice"> <?php  echo $Price; ?> </span>
   </section>
   <section class="list-right">
-  <span class="date"><?php echo $Release_date ?></span>
-  <span Class="contact email"><?php echo $Email ?></span>
-  <span Class="contact phone number"> <?php echo $Phone_number ?></span>
-
+  <span class="date"><?php echo $Release_date; ?></span>
+  <span Class="contact email"> xxxxx@buffalo.edu</span>
+  <span Class="contact phone number"> xxxx-xxx-xx67</span>
+  <?php echo $id ?>
   </section>
   <div class="clearfix"></div>
   </li>
