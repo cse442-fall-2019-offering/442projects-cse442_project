472fac1d6649d2a451dc123dc468a093aa956427
xianxinl <31662429+xianxinl@users.noreply.github.com>
2019-10-23 20:25:36 -0400

    Update dele_post.php
    redirect to manage page after deleting

--- dele_post.php
+++ dele_post.php
@@ -1,18 +1,22 @@
 <?php
   session_start();
-  require_once('setting/local_host_config.php');
+  require_once('setting/server_config.php');
   $Product_Table_name= "product";
   $Account_Table_name= "useraccount";
 
   if(!$conn){
    echo ("Fail to conncet server".mysqli_connect_error());
   }
-  $pid = $_GET['Product_ID'];
+  else {
+      $pid = $_GET['Product_ID'];
+  }
+
+
+
   $sql= "DELETE FROM $Product_Table_name WHERE id= '$pid'";
   $result= $conn ->query($sql);
   unset($_GET['Product_ID']);
-  header('Location: index.php');
-
+  header('Location: manage.php');
 
 
 
