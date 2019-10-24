9cbf3c8fbe2c1c495cc7d48b1b23f1f78b517ecf
Junlong Ye <junlongfighting@gmail.com>
2019-10-24 01:38:34 -0400

    remove unnecessary code #56
    remove unnecessary code

--- dele_post.php
+++ dele_post.php
@@ -2,7 +2,6 @@
   session_start();
   require_once('setting/server_config.php');
   $Product_Table_name= "product";
-  $Account_Table_name= "useraccount";
 
   if(!$conn){
    echo ("Fail to conncet server".mysqli_connect_error());
@@ -17,8 +16,6 @@
   $result= $conn ->query($sql);
   unset($_GET['Product_ID']);
   header('Location: manage.php');
-
-
 
 
 
