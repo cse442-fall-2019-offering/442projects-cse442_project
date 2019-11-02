adbb9d785dbcebb951234e6ee9f13e55d7766ff5
xianxinl <31662429+xianxinl@users.noreply.github.com>
2019-11-01 21:51:48 -0400

    Update index.php
    1. Adding "sort by" dropdown bar
    2. adding three "sort by" ways: "Release date", "price from high to low" " price from low to high"
    3. setting default "sort by" as "Release date" 
    4. achieve the resort by getting "sort way" from $_GET['orderby"] and refresh page.

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <?php
   require_once('setting/server_config.php');
+  session_start();
  ?>
+
 <html lang="en">
 <head>
 <title>UB TRADE</title>
@@ -13,8 +15,19 @@
 		<div class="w3ls-header"><!--header-one-->
 			<div class="w3ls-header-right">
 				<ul>
+          <!-- click to sign out account -->
+          <li class="dropdown head-dpdn">
+            <a href="setting/Sign_Out.php"><i aria-hidden="true"></i> Sign Out</a>
+          </li>
+          <!-- sign out end -->
 					<li class="dropdown head-dpdn">
-						<a href="signin.html" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i> Sign In</a>
+            <!-- replace login in by username when logined in !-->
+						<a href="signin_front.php" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i>
+              <?php if (isset($_SESSION['username']))
+    				{
+        		echo $_SESSION['username'];
+    			} else echo"Sign In"?> </a>
+          <!-- username display end !-->
 					</li>
 					<li class="dropdown head-dpdn">
 						<a href="help.html"><i class="fa fa-question-circle" aria-hidden="true"></i> Help</a>
@@ -60,30 +73,53 @@
 						<div role="tabpanel" class="tab-pane fade in active" id="home" aria-labelledby="home-tab">
 						   <div>
 												<div id="container">
+                          <div class="sort">
+                               <div class="sort-by">
+                                <label>Sort By : </label>
+                                <form method="GET">
+                                <select name = "orderby" onchange="this.form.submit()">       <!-- every time when select, pass the "order way" to get and refresh page with updated order way-->
+                                        <option hidden><?php if(isset($_GET['orderby'])){
+                                          if($_GET['orderby'] =="Release_date DESC" )  {echo "Most recent";}
+                                          else if ($_GET['orderby'] =="Price DESC" ) {echo "Price: High to Low";}
+                                          else if ($_GET['orderby'] =="Price ASC" ) {echo "Price: Low to High";}
+                                        } else echo"Most recent";
+                                        ?></option>
+                                        <option value="Release_date DESC">Most recent</option>
+                                        <option value="Price ASC">Price: Low to High</option>
+                                        <option value="Price DESC">Price: High to Low</option>
+                                </select>
+                              </form>
+                                 </div>
+                               </div>
 
 								<div class="clearfix"></div>
+              <div id ="allsection">
 							<ul class="list">
 <!--implementing display_method:
   display all products in database to front site with its' information
   in section format-->
 								<?php
-                      $query = "SELECT * FROM product";
+                if(isset($_GET['orderby'])){       //set the default order way to be "recent_upload"
+                $ordby = $_GET['orderby'];
+              } else { $ordby = "Release_date DESC"; }
+                $query = "SELECT * FROM product ORDER BY $ordby";;
                 $result = mysqli_query($conn, $query);
                 while($row = mysqli_fetch_assoc($result)){
                   $id =$row['id'];
                   $Product_Name = $row['Product_Name'];
                   $Price = $row['Price'];
                   $Release_date = $row['Release_date'];
-                  $Image_location = $row['Image_location'];
+                  $Image = $row['Image'];
+                  $Phone_number = $row['Phone_number'];
+                  $Email = $row['Email'];
+                  $Product_ID = $row['id'];
                   $Product_description = $row['Product_description'];
-                    include 'single_product_section.php';
+                  include 'single_product_section.php';
                 }
-
-
-
 
                  ?>
 							</ul>
+            </div>
 						</div>
 							</div>
 						</div>
