1880150bb92d591ce075512cec7a54b4cc927e2b
xianxinl <31662429+xianxinl@users.noreply.github.com>
2019-11-10 20:26:43 -0500

    Update single_product_section_del.php
    "edit" button added
    connect with update.php

--- single_product_section_del.php
+++ single_product_section_del.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<style>
+test {
+     width: 100%;
+     height: 100%;
+}
+</style>
 <a>
         <li>
           <img src=<?php echo "images/".$Image ?> title="" alt="" />
@@ -6,7 +13,13 @@
             <span Class="Product_description"><?php echo 	$Product_description ?> </span>
             <span class="adprice"> <?php  echo "$".$Price ?> </span>
             </section>
-            <section class="list-right">                    <!--only the user can delete post !-->
+            <section class="list-right">
+              <form class="edit_form" method="GET" action= <?php if (isset($_SESSION['username'])){
+                echo"update.php"; }?>>
+                <input type="hidden" name="Product_ID" value = <?php echo $Product_ID ?> />
+                <button>Edit</button>
+              </form>
+                                <!--only the user can delete post !-->
               <form class="dele_form" method="GET" action= <?php if (isset($_SESSION['username'])){
                 echo"dele_post.php"; }?>>
                 <input type="hidden" name="Product_ID" value = <?php echo $Product_ID ?> />
